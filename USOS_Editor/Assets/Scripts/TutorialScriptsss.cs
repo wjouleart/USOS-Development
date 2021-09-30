16e23a89e0a80e134f12e0e1392bab11f1f6da38
jiayi99 <jiayi19991@gmail.com>
2021-09-30 22:25:54 +0800

    Test

---
+++ USOS_Editor/Assets/Scripts/TutorialScriptsss.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScriptsss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hihihihihihihi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
