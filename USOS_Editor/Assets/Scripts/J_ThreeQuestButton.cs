0e3c3dee5b09e8f0685dd82487a3ba5b6d8046b4
jiayi99 <jiayi19991@gmail.com>
2021-10-03 17:15:46 +0800

    Updated Quest UI, PauseMenu

--- USOS_Editor/Assets/Scripts/J_ThreeQuestButton.cs
+++ USOS_Editor/Assets/Scripts/J_ThreeQuestButton.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class J_ThreeQuestButton : MonoBehaviour
 {
     public GameObject PSP, solarwind, sun;
-   public void FirstQuest()
+    public GameObject PauseMenu;
+    public GameObject YesorNo;
+    public GameObject Infoo;
+    public void FirstQuest()
     {
         PSP.SetActive(true);
     }
@@ -17,5 +21,44 @@ public class J_ThreeQuestButton : MonoBehaviour
     public void ThirdQuest()
     {
         sun.SetActive(true);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+    }
+
+    public void YesOrNo()
+    {
+        YesorNo.SetActive(true);
+        PauseMenu.SetActive(false);
+
+    }
+
+    public void infoo()
+    {
+        Infoo.SetActive(true);
+    }
+
+    public void Backbutton()
+    {
+        Infoo.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    public void Yes()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void No()
+    {
+        YesorNo.SetActive(false);
+        PauseMenu.SetActive(true);
     }
 }
